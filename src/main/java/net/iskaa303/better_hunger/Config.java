f887f4c2979b85f4ee4bc8f0303782fe47d4efb3
Iskaa303 <iska060808@gmail.com>
2025-09-09 18:22:24 -0400

    Clean up files and add LICENSE

---
+++ src/main/java/net/iskaa303/better_hunger/Config.java
@@ -0,0 +1,26 @@
+package net.iskaa303.better_hunger;
+
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.common.ForgeConfigSpec;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.config.ModConfigEvent;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Mod.EventBusSubscriber(modid = BetterHunger.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
+public class Config
+{
+    private static final ForgeConfigSpec.Builder BUILDER = new ForgeConfigSpec.Builder();
+
+    static final ForgeConfigSpec SPEC = BUILDER.build();
+
+    @SubscribeEvent
+    static void onLoad(final ModConfigEvent event)
+    {
+    }
+}
